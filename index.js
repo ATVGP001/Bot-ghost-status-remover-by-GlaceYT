a5f21ba6002b6706c6a23188051f213b5c480edc
ATVGP001 <166093079+ATVGP001@users.noreply.github.com>
2024-09-08 07:32:34 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Playing FreeFire","Watching Live Of Azur"];
 
 
 let currentIndex = 0;
